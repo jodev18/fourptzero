4b4cd32dfae2bbb4e555b7005e7876bfc1e218ca
myxroft <josiahbuhayan@gmail.com>
2021-04-01 11:57:18 +0800

    added session validation

--- prob2_backend/dashboard.php
+++ prob2_backend/dashboard.php
@@ -1,4 +1,4 @@
-<?php session_start(); ?>
+<?php session_start(); if(empty($_SESSION['curr_userid'])){echo "<script> window.location.href = \"index.php\"; </script>";}?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
--- prob2_backend/db/logout.php
+++ prob2_backend/db/logout.php
@@ -1,3 +1,5 @@
-<?php session_destroy();
+<?php session_start();
+session_destroy();
+session_unset();
 echo "<script> alert('Logged out.'); </script>";
 echo "<script> window.location.href = \"../index.php\"; </script>"; ?>
